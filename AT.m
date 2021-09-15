e5ffef4d07fddf182e124c3836a0d6bc330f6f17
zejiang-unsw <47626771+zejiang-unsw@users.noreply.github.com>
2021-09-15 17:22:23 +1000

    Create AT.m

---
+++ AT.m
@@ -0,0 +1,17 @@
+function  [X_AT] = AT(X, wname, lev)
+
+
+  s = nan(length(X),lev);
+  for i = 1:lev
+    tmp = (modwt(X, wname, i))' ;
+    s(:,i) = tmp(:,i+1) ;
+  end
+
+  X_AT = nan(length(X),lev+1);
+  X_AT(:,1) = X-s(:,1); 
+  for i = 1:(lev-1) 
+      X_AT(:,i+1) = s(:,i)-s(:,i+1) ;
+  end
+  X_AT(:,lev+1) =  s(:,lev); 
+
+end
