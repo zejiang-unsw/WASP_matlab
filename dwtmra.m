ac22d4731aeb96efcb2c8f0546cc2d7c2f1fb161
zejiang-unsw <47626771+zejiang-unsw@users.noreply.github.com>
2021-09-15 17:21:40 +1000

    Create dwtmra.m

---
+++ dwtmra.m
@@ -0,0 +1,16 @@
+function [X_DWT_MRA] = dwtmra(X, wname, lev)
+    
+    N = length(X); 
+    
+    %1-D wavelet decomposition
+    [C,L] = wavedec(X,lev,wname); 
+
+    X_DWT_MRA = nan(N,lev+1); 
+    for i=1:lev
+      X_DWT_MRA(:,i) = wrcoef('d',C,L,wname,i);
+    end
+    X_DWT_MRA(:,lev+1) = wrcoef('a',C,L,wname,lev);
+      
+%     disp(['Additive:' num2str(sum(abs(sum(X_DWT_MRA,2)-X)))])
+%     disp(['Variance:' num2str(sum(var(X_DWT_MRA))-var(X))])
+end 
