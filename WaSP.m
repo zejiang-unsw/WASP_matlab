95a09bcb170054bec373ab2a6bbcdccc4cf1765e
Ze Jiang <zejiang@gfz-potsdam.de>
2024-01-22 11:08:00 +1100

    WASP matlab v1.0

--- WaSP.m
+++ WaSP.m
@@ -52,46 +52,25 @@ for i_var = 1 : num_var
         disp('This type of wavelet transform does not apply!')
     end
     
-%     disp(['Additive:' num2str(sum(abs(sum(X_WT,2)-X_tmp)))])
-%     disp(['Variance:' num2str(sum(var(X_WT))-var(X_tmp))])
- 
     %standardization w.r.t. observed period
     X_WT_n = X_WT(1:N,:);
-    %X_WT_norm=(X_WT-mean(X_WT_n))./std(X_WT_n);
     
     % alternative way to standardize data for older matlab versions
-    %subtract mean of each column
+    % subtract mean of each column
     X_WT_c=X_WT-repmat(mean(X_WT_n),num_obs,1);
-    %divide by the standard deviation of each column
+    % divide by the standard deviation of each column
     X_WT_norm=X_WT_c./repmat(std(X_WT_n,0,1),num_obs,1);
-%     disp(sum(abs(X_WT_norm1-X_WT_norm)))   
+
     
-%     disp(var(X_WT_norm(1:N,:)))  
-%     disp(mean(X_WT_norm(1:N,:))) 
-%     disp(sum(abs(normalize(X_WT_n)-X_WT_norm(1:N,:))))
-
-    %corr1 = corrcoef([Y X_WT(1:length(Y),:)]) ; %correct
-    %corr1 = cov([Y X_WT(1:length(Y),:)]) ; %wrong
-    %C(:,i_var) = corr1(1,2:lev+2); 
-
     % covariance - Eq. 10 in WRR2020 paper
-    %corr = 1/(N-1)*Y'*X_WT_norm(1:N,:); 
-
-    %corr = 1/(length(Y)-1)*Y'*normalize(X_WT(1:length(Y),:)); 
-    %disp(normalize(corr1(1,2:lev+2),'norm') - normalize(corr,'norm'))
     C(:,i_var) = 1/(N-1)*Y'*X_WT_norm(1:N,:); 
     
     % normalization to get unit norm
-    %C_norm = normalize(C(:,i_var),'norm') ;
     C_norm = C(:,i_var)./sqrt(sum(C(:,i_var).^2)) ;
-%     disp(norm(C_norm))
-%     disp(norm(C(:,i_var))-sqrt(sum(C(:,i_var).^2)))
     
     % variance transformation -  Eq. 9 in WRR2020 paper
-    %X_WaSP(:,i_var) = normalize(X_WT)*(std(X(:,i_var)).*C_norm(:)) ; 
     X_WaSP(:,i_var) = X_WT_norm*(std(X(1:N,i_var)).*C_norm) ; 
     
-    X_WT_norm'*X_WT_norm
 	% maintain the original trend of the variable
 	if flag_sign
 		[rho, pval] = corr(X_WaSP(:,i_var),X_tmp); 
