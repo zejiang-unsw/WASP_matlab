e4f14d35022f6eb56a01db77ff5a1d45fbb72008
Ze Jiang <zejiang@gfz-potsdam.de>
2024-07-23 20:35:29 +1000

    Update Example1_WaSP.m
    - update synthetic example

--- Example1_WaSP.m
+++ Example1_WaSP.m
@@ -1,5 +1,4 @@
-clc
-clear
+clc; clear
 close all
 
 % Created by Ze Jiang on 15/09/2021 (ze.jiang@unsw.edu.au)
@@ -7,26 +6,30 @@ close all
 % X: predictor= (N+N_fc) x n_var
 
 N = 128;         % number of observation
-N_fc=0;         % number of forecast (optional)
+N_fc=24;         % number of forecast (optional)
 n_var=4;        % number of variable
-iseed = 101;    % seed number for random number generator
 
-%% initialize random number generator
-rng(iseed,'twister')
+% initialize random number generator
+%iseed = 101;    
+%rng(iseed,'twister')
 
 %% synthetic data generation
-t = linspace(-pi,pi,N);
-Y = (sin(t) + 1.0*randn(size(t)))' %+ t'; % sine wave add noise
+%t = linspace(-pi,pi,N+N_fc);
+fs = 50;
+dt = 1/fs;
+t = 0:dt:dt*(N+N_fc-1); 
+Y_ALL = (sin(2*pi*t+randn(1,1)) + 0.1*randn(size(t)))'; %+ t'; % sine wave add noise and trend
 
-X = randn(N+N_fc,n_var) %- repmat(linspace(-pi,pi,N+N_fc)',1, n_var); % random predictors
+X = randn(N+N_fc,n_var);% + repmat(linspace(-pi,pi,N+N_fc)',1, n_var); % random predictors and trend
 % X = [randn(N,n_var); repmat(100,N_fc,n_var)]; % test on abnormal value from forecast
+
+Y = Y_ALL(1:N); Y_val = Y_ALL(N+1:end); 
 
 %% Daubechies wavelet with N vanishing moments 
 % N is a positive integer from 1 to 45
 n_vanish = 2; 
-wname = ['db' num2str(n_vanish)] % db1 is equivalent to haar
-flag_sign=0;
-
+wname = ['db' num2str(n_vanish)] % db1 is equivalent to haar; 'sym2'; 'bior2.6'
+flag_sign = 1;
 
 %% method of discrete wavelet transform 
 switch 1
@@ -35,12 +38,14 @@ switch 1
     case 2
         method = 'modwt'
     case 3 
+        method = 'modwtmra'
+    case 4 
         method = 'at'
 end
 
 % maximum decomposition level: floor(log2(size(X,1)))
 % or rule of thumb decomposition level: ceiling(log(n/(2*v-1))/log(2))-1 (Kaiser, 1994)
-lev = floor(log2(size(X,1)))-1 ; 
+lev = floor(log2(size(X,1)))-1 
 
 % variance transformation using WaSP
 [X_WaSP, C] = WaSP(Y, X, method, wname, lev, flag_sign); 
@@ -56,17 +61,19 @@ for i_var = 1:n_var
     RMSE_opti(i_var) = sqrt((N-1)/N*(var(Y)-(norm(C(:,i_var))^2)*ratio)); 
     
     % Std model: linear regression using original predictor
-    p_coeff = polyfit(X(1:N,i_var), Y, 1)  ;
-    Y_fit = polyval(p_coeff, X(1:N, i_var)) ; 
+    p_coeff1 = polyfit(X(1:N,i_var), Y, 1)  ;
+    Y_fit = polyval(p_coeff1, X(1:N, i_var)) ; 
     RMSE(i_var) = sqrt(mean((Y-Y_fit).^2));
     
     % VT model: linear regression using transformed predictor
-    p_coeff = polyfit(X_WaSP(1:N,i_var), Y, 1)  ;
-    Y_fit = polyval(p_coeff, X_WaSP(1:N, i_var)) ; 
+    p_coeff2 = polyfit(X_WaSP(1:N,i_var), Y, 1)  ;
+    Y_fit = polyval(p_coeff2, X_WaSP(1:N, i_var)) ; 
     RMSE_WaSP(i_var) = sqrt(mean((Y-Y_fit).^2));
 
 end
 
+RMSE_WaSP
+RMSE_opti
 %% plot RMSE
 figure
 bar([RMSE',RMSE_WaSP',RMSE_opti']);
@@ -82,7 +89,7 @@ for i_var = 1:n_var
     subplot(n_var,1,i_var)
     
     plot(Y, 'k')     
-    xlim([0,N+N_fc]); ylim([min(Y)*1.1,max(Y)*1.1]); 
+    xlim([0,N+N_fc]); %ylim([min(Y)*1.1,max(Y)*1.1]); 
     hold on 
     plot(X(:,i_var), 'b')
     hold on
@@ -91,23 +98,3 @@ for i_var = 1:n_var
     legend('Y','X','Xnew','NumColumns',1,'location','eastoutside')      
 end 
 saveas(gca,'comparision.fig');
-
-
-%% validation
-X_val=randn(N,n_var); % random predictors
-
-% variance transformation using derived C from calibration period
-X_WaSP_val = WaSP_val(X_val, C, method, wname,flag_sign); 
-
-figure
-sgtitle(['Validation: ',num2str(method) ' using ' num2str(wname)])
-for i_var = 1:n_var
-    subplot(n_var,1,i_var)
-    
-    plot(X_val(:,i_var), 'b')
-    hold on
-    plot(X_WaSP_val(:,i_var), 'r')     
-    hold off
-    legend('X','Xnew','NumColumns',1,'location','eastoutside')      
-end 
-saveas(gca,'comparision_val.fig');
